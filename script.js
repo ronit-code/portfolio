93e97fe624c0f8562ad139465562cced138aa35d
ronit-code <128163306+ronit-code@users.noreply.github.com>
2024-07-31 23:44:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
   function Redirect() {
     location.href="https://github.com/ronit-code?tab=repositories";
   }
+
+
+function downloadFile() {
+  const link = document.createElement("a");
+  link.href = "resume.pdf"
+  link.download = '';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+}
